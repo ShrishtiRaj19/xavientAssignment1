7c8243c99bd2c3b83e9f883e6526908ae85ec47a
shrishtiRaj19 <rajshrishti94@gmail.com>
2020-04-13 19:21:58 +0530

    db changes

--- models/index.js
+++ models/index.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 const db_url = process.env.MONGODB_URL;
+console.log("db_url", db_url)
 mongoose.connect(db_url, {
 	keepAlive: true,
   	useNewUrlParser: true,
   	useUnifiedTopology: true
+}, (err, res)=>{
+	console.log("err", err. res)
 })
 
 mongoose.set("debug", true) 
